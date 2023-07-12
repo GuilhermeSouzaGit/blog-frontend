d58e3cc0bb3cc5ba3f5e91cc3d08197cf17eaa4e
Guilherme <guisou1902@gmail.com>
2023-07-11 22:40:30 -0300

    feat: added a component for post

---
+++ src/pages/Post/Post.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { UniquePost } from "../../components/UniquePost/UniquePost";
+
+export const Post = () => {
+	return (
+		<div className="post-container">
+			<UniquePost />
+		</div>
+	);
+};
