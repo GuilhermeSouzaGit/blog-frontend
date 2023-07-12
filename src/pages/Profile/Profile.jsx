176c8c24e6d791b35d5f3a99c9cdfdfdecd9c165
Guilherme <guisou1902@gmail.com>
2023-07-11 22:40:00 -0300

    feat: added a profile component

---
+++ src/pages/Profile/Profile.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { UserProfile } from "../../components/UserProfile/UserProfile";
+
+export const Profile = () => {
+	return (
+		<>
+			<h1>Página Profile</h1>
+			<UserProfile />
+		</>
+	);
+};
