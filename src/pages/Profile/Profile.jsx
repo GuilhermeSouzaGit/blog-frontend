bedac65d699e7feaade850013d370b78ddd22804
Guilherme <guisou1902@gmail.com>
2023-07-20 18:43:14 -0300

    feat: added navbar component and removed h1 element

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { UserProfile } from "../../components/UserProfile/UserProfile";
+import { Navbar } from "../../components/Navbar/Navbar";
 
 export const Profile = () => {
 	return (
 		<>
-			<h1>Página Profile</h1>
+			<Navbar />
 			<UserProfile />
 		</>
 	);
