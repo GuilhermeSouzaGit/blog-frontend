79c812741a08c4e752908a44b1ccb30dab963261
Guilherme <guisou1902@gmail.com>
2023-07-20 18:42:01 -0300

    fix: removed unnecessary div

--- src/pages/Post/Post.jsx
+++ src/pages/Post/Post.jsx
@@ -2,9 +2,5 @@ import React from "react";
 import { UniquePost } from "../../components/UniquePost/UniquePost";
 
 export const Post = () => {
-	return (
-		<div className="post-container">
-			<UniquePost />
-		</div>
-	);
+	return <UniquePost />;
 };
