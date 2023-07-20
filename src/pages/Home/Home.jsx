b30fbf5f0adb7540b307ad042d06387bc1cd610a
Guilherme <guisou1902@gmail.com>
2023-07-20 18:38:18 -0300

    feat: added some call to actions in homepage

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import { Navbar } from "../../components/Navbar/Navbar";
+import "./Home.css";
+import { Link } from "react-router-dom";
 
 export const Home = () => {
 	return (
 		<div>
 			<Navbar linkRoute="/login" btnText="Entrar" />
+			<div className="call-to-action">
+				<h1 className="cta-title">Ainda não tem uma conta?</h1>
+				<h1>Crie uma agora mesmo!</h1>
+				<Link to="/register">
+					<button className="btn btn-cta">Registrar-se</button>
+				</Link>
+			</div>
 		</div>
 	);
 };
